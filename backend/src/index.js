f831ada4c0cdecdb2eb7c8d54877de544624084b
karthik2003-cpu <karthikrckm2003@gmail.com>
2025-02-21 11:17:10 +0530

    added routes and connected to mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv';
+import {connectDB} from './lib/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/api/auth', authRoutes);
 
-app.get('/welcome', (req, res) => {
-    res.send('welcome to the world of express');
-});
+const PORT = process.env.PORT
 
-app.listen(5002, () => {
-    console.log("Server is running on port 5002");
+app.listen(PORT, () => {
+    console.log("Server is running on port:"+ PORT);
+    connectDB();
 });
