849956f1eafdcb6201eb18106cafbaedee660f56
karthik2003-cpu <karthikrckm2003@gmail.com>
2025-02-24 16:40:54 +0530

    tested all api endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,21 @@
-import express from 'express';
-import authRoutes from './routes/auth.routes.js';
-import dotenv from 'dotenv';
-import {connectDB} from './lib/db.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log("Server is running on port:"+ PORT);
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log("Server is running on port: "+port);
     connectDB();
 });
